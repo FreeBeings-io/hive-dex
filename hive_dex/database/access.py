bed40ca5a4236a2a25c45b5acc9be78a1eb828b7
imwatsi <imwatsi@freebeings.io>
2022-10-28 00:00:00 +0400

    add db access and haf

---
+++ hive_dex/database/access.py
@@ -0,0 +1,20 @@
+from hive_dex.database.core import DbSession
+from hive_dex.tools import populate_by_schema, normalize_types
+
+_read_db = DbSession("read")
+
+def select_raw(sql:str):
+    _res = _read_db.do('select',sql)
+    return _res
+
+def select(sql:str, schema:list, one:bool = False):
+    _res = _read_db.do('select',sql)
+    res = []
+    if _res:
+        assert len(schema) == len(_res[0]), 'invalid schema'
+        for x in _res:
+            res.append(populate_by_schema(x,schema))
+        if one:
+            return normalize_types(res)[0]
+        else:
+            return normalize_types(res)
---
+++ hive_dex/database/haf.py
@@ -0,0 +1,90 @@
+import os
+import re
+from threading import Thread
+from hive_dex.config import Config
+from hive_dex.database.core import DbSession
+
+START_DAYS_DISTANCE = 1
+SOURCE_DIR = os.path.dirname(__file__) + "/sql"
+
+
+config = Config.config
+
+
+class Haf:
+
+    module_list = []
+
+    @classmethod
+    def _get_haf_sync_head(cls, db):
+        sql = "SELECT hive.app_get_irreversible_block();"
+        res = db.do('select', sql)
+        return res[0]
+
+    @classmethod
+    def _is_valid_module(cls, module):
+        return bool(re.match(r'^[a-z]+[_]*$', module))
+    
+    @classmethod
+    def _update_functions(cls, db, functions):
+        db.do('execute', functions, None)
+        db.do('commit')
+
+    @classmethod
+    def _init_hive_dex(cls, db):
+        db.do('execute', f"CREATE SCHEMA IF NOT EXISTS {config['schema']};")
+        db.do('commit')
+        for _file in ['tables.sql', 'functions.sql', 'sync.sql']:
+            _sql = (open(f'{SOURCE_DIR}/{_file}', 'r', encoding='UTF-8').read()
+                .replace('hive_dex.', f"{config['schema']}.")
+            )
+            db.do('execute', _sql)
+        db.do('commit')
+        has_globs = db.do('select', f"SELECT * FROM {config['schema']}.global_props;")
+        if not has_globs:
+            db.do('execute', f"INSERT INTO {config['schema']}.global_props (check_in) VALUES (NULL);")
+            db.do('commit')
+    
+    @classmethod
+    def _prepare_data(cls, db):
+        has_prepped = db.do('select', f"SELECT * FROM {config['schema']}.pairs;")
+        if not has_prepped:
+            db.do('execute',f"INSERT INTO {config['schema']}.pairs VALUES ('HIVE_HBD', 'HIVE', 'HBD')")
+            db.do('commit')
+    
+    @classmethod
+    def _init_main_sync(cls, db):
+        print("Starting main sync process...")
+        db.do('execute', f"CALL {config['schema']}.sync_main();")
+    
+    @classmethod
+    def _cleanup(cls, db):
+        """Stops any running sync procedures from previous instances."""
+        running = db.do('select_one', f"SELECT {config['schema']}.is_sync_running('{config['schema']}-main');")
+        if running is True:
+            db.do('execute', f"SELECT {config['schema']}.terminate_main_sync('{config['schema']}-main');")
+        cmds = [
+            f"DROP SCHEMA {config['schema']} CASCADE;",
+        ]
+        if config['reset'] == 'true':
+            for cmd in cmds:
+                try:
+                    db.do('execute', cmd)
+                    db.do('commit')
+                except Exception as err:
+                    print(f"Reset encountered error: {err}")
+            cls._init_hive_dex(db)
+
+    @classmethod
+    def init(cls, db):
+        """Initializes the HAF sync process."""
+        cls._init_hive_dex(db)
+        cls._cleanup(db)
+        cls._prepare_data(db)
+        start = db.do('select', f"SELECT {config['schema']}.global_sync_enabled()")[0][0]
+        if start is True:
+            db_main = DbSession('main')
+            Thread(target=cls._init_main_sync, args=(db_main,)).start()
+        else:
+            print("Global sync is disabled. Shutting down")
+            os._exit(0)
