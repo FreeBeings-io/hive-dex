1334c2fa7502e1e6e59b8e657fa0600b7345f4bc
imwatsi <imwatsi@freebeings.io>
2022-10-27 23:59:44 +0400

    add db schema

--- hive_dex/database/sql/tables.sql
+++ hive_dex/database/sql/tables.sql
@@ -1,25 +1,43 @@
 CREATE TABLE IF NOT EXISTS hive_dex.global_props(
     latest_block_num INTEGER,
-    latest_block_time TIMESTAMP,
     check_in TIMESTAMP,
     sync_enabled BOOLEAN DEFAULT true
 );
 
+CREATE TABLE IF NOT EXISTS hive_dex.pairs(
+    pair_id VARCHAR(9) PRIMARY KEY,
+    base VARCHAR(4),
+    target VARCHAR(4)
+);
+
 CREATE TABLE IF NOT EXISTS hive_dex.orders(
-    order_id BIGINT,
+    id BIGSERIAL PRIMARY KEY,
+    trx_id BYTEA,
+    pair_id VARCHAR(9) REFERENCES hive_dex.pairs(pair_id),
     acc VARCHAR(16),
-    sell_amount INTEGER,
-    sell_nai VARCHAR(11),
-
+    order_id BIGINT,
+    side VARCHAR(1),
+    hbd BIGINT,
+    hive BIGINT,
+    settled BIGINT DEFAULT 0,
     fill_or_kill BOOLEAN,
-    expires TIMESTAMP,
-    cancelled BOOLEAN,
+    expires TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS hive_dex.orders_cancelled(
-    order_id BIGINT NOT NULL REFERENCES hive_dex.orders(order_id),
-    
+CREATE TABLE IF NOT EXISTS hive_dex.trades(
+    id BIGSERIAL PRIMARY KEY,
+    block_num INTEGER,
+    block_time TIMESTAMP,
+    trx_id BYTEA,
+    current_owner VARCHAR(16),
+    current_amount BIGINT,
+    current_nai VARCHAR(11),
+    open_owner VARCHAR(16),
+    open_amount FLOAT,
+    open_nai VARCHAR(11)
 );
 
-hive_dex.trades
+CREATE INDEX IF NOT EXISTS idx_hdx_orders_expires
+    ON hive_dex.orders (expires);
+
 
