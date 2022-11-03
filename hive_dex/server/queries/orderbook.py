9d2e733a898497d2142dfbd858f696c7c15006bd
imwatsi <imwatsi@freebeings.io>
2022-11-03 16:40:46 +0400

    adjust orderbook queries

--- hive_dex/server/queries/orderbook.py
+++ hive_dex/server/queries/orderbook.py
@@ -4,15 +4,16 @@ def get_orderbook_buys(depth:int):
     sql_buys = f"""
         SELECT
             (
-                round(pays::numeric/receives::numeric, 6)
+                round((pays::numeric/receives::numeric)::numeric(12,6), 6)
             )::varchar price,
             (
-                round((receives::numeric)/1000, 3)
-            )::varchar hive
+                SUM(round((receives::numeric)/1000, 3)
+            ))::varchar hive
         FROM dev.orders
         WHERE pays_nai = '@@000000013'
             AND settled < pays
             AND expires > NOW() AT TIME ZONE 'utc'
+        GROUP BY price
         ORDER BY price DESC
         LIMIT {depth};
     """
@@ -22,15 +23,16 @@ def get_orderbook_sells(depth:int):
     sql_sells = f"""
         SELECT
             (
-                round((receives::numeric/pays), 6)
+                round((receives::numeric/pays::numeric)::numeric(12,6), 6)
             )::varchar price,
             (
-                round((pays::numeric)/1000, 3)
-            )::varchar hive
+                SUM(round((pays::numeric)/1000, 3)
+            ))::varchar hive
         FROM dev.orders
         WHERE pays_nai = '@@000000021'
             AND settled < pays
             AND expires > NOW() AT TIME ZONE 'utc'
+        GROUP BY price
         ORDER BY price ASC
         LIMIT {depth};
     """
